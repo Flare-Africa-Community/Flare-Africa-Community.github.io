bc01440ba1d3d2f9ea1abdc09530dbe4d6ce1199
ESE-MONDAY <mondayese8@gmail.com>
2026-01-08 08:11:35 +0100

    Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
+
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
     vite: {
     plugins: [tailwindcss()],
   },
+  env: {
+    schema: {
+      PUBLIC_TRACKING_ID: envField.string({ context: 'client', access: 'public' }),
+    }
+  },
   site: 'https://Flare-Africa-Community.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), partytown({
+    config:{
+      forward: ['dataLayer.push'],
+    }
+  })],
 });
